e87334c638bf9c11cef06321e264150a46008d07
Megakuul <megakuul@gmx.ch>
2023-12-20 23:21:32 +0100

    Updated bazel rules + added rules_boost

--- hypercache_db/src/main.cc
+++ hypercache_db/src/main.cc
@@ -2,7 +2,11 @@
 
 #include "main.h"
 
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
+
 using namespace std;
 int main(void) {
+  boost::asio::io_context io_context;
   cout << "Hallo From " << TEST << endl;
 }
