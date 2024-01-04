fdfb257e6b828e63f8809ff4f7e2c13ba3fee9ab
Megakuul <megakuul@gmx.ch>
2024-01-04 16:59:25 +0100

    Implemented basic operations for hypermap

--- src/lib/hypermap/hypermap.hpp
+++ src/lib/hypermap/hypermap.hpp
@@ -27,9 +27,6 @@
 
 #include "hyperhash.hpp"
 
-// Number of slots in the hashmap
-#define MAP_SIZE 16384 // Must be a size that is powered by 2
-
 using namespace std;
 
 namespace hypermap {
@@ -48,11 +45,38 @@ namespace hypermap {
 		}
 	};
 
+	/**
+	 * HyperMap
+	 *
+	 * Open addressing hashmap that is optimized for read/write speed using quadratic probing.
+	 * Tradeoff for this is that the map preallocates every slot with the largest type in the Derived_T types.
+	 *
+	 * Usage:
+	 * HyperMap template requires a "Base_T" type as first template argument and a list of "Derived_T" types as second argument.
+	 * The "Base_T" type must be the base class for the "Derived_T" types.
+	 * System works like this: Every slot (bucket) can be set to one of the derived types, you can change the types in the slot
+	 * at runtime by calling hypermap.set("key", ...) or by using the hypermap["key"] = ... overloader.
+	 *
+	 * The hypermap.get("key") function will return a "Base_T" pointer. This is then usually used through a virtual function that is overriden by the derived classes.
+	 * For regularly manipulating elements (like e.g. updating values), use hypermap.get("key") and directly work with the element (use hypermap.set("key", ...) for type changing or initial allocation).
+	 *
+	 * Considerations:
+	 * - The "mapsize" MUST be a power of two, this is required for correct hash-trimming. Initialization will throw an invalid_argument error if it's not.
+	 * - HyperMap will preallocate "mapsize" buckets that are all sized like the largest type in "Derived_T".
+	 * - The size of the map is immutable, the performance of the map will starve if the load is too high.
+	 * - All "Derived_T" types must be statically upcastable to "Base_T".
+	 * - All "Derived_T" types / their members must implement correct copy/move semantics (just so that the type can be deep copied and moved with "=")
+	 *
+	 */
 	template <typename Base_T, typename... Derived_T>
 	class HyperMap {
 	public:
-		HyperMap() {
-			size = MAP_SIZE;
+		HyperMap(uint16_t mapsize) {
+			// Check if map is power of 2
+			if (!(mapsize > 0 && (mapsize & (mapsize-1)) == 0))
+				throw invalid_argument("Mapsize must be a power of two!");
+			// Initialize map
+			size = mapsize;
 			map = new HyperSlot<variant<Derived_T...>>[size];
 		};
 		virtual ~HyperMap() {
@@ -67,20 +91,23 @@ namespace hypermap {
 			};
 		};
 		HyperMap(const HyperMap& other) : size(other.size) {
-			// TODO: Implement the full copy constructor (kinda struggling with teh allocation (new[] would init them, what is a unnecessary overhead, operator new[] on the other hand cannot be deallocated with the destructor (because they don't want to be deallocated with delete[] map, but with operator delete[](map);
-			// Skip if same
 			if (this != &other) {
 				try {
-				// Allocate block without initialization
-				map = static_cast<HyperSlot<variant<Derived_T...>>*>(
-					  operator new[](other.size * sizeof(HyperSlot<variant<Derived_T...>>))
-			  );
-				// Initialize every field with copy constructor
-				for (size_t i = 0; i < other.size; ++i) {
-					new (&map[i]) HyperSlot<variant<Derived_T...>>(other.map[i]);
-				}
+					// Copy constructor in this case uses default initialization and assignment instead of the copy constructor of the elements.
+					// Technically it is feasible to use "operator new[] / delete[]"; This would allow direct construction of elements (e.g. direct copy construction).
+					// But for performance reasons, the memory management of the HyperMap is handled with high-level "new[] and delete[]" functions (which can be quite more efficient then manual management).
+					// HyperMap is primarly specified for fast access times and not for memory optimizations; it is usually not necessary to COPY the HyperMap.
+					
+					// Allocate block
+					map = new HyperSlot<variant<Derived_T...>>[other.size];
+					// Initialize every field with assignment overloader
+					for (size_t i = 0; i < other.size; ++i) {
+						map[i] = other.map[i];
+					}
 				} catch (...) {
-
+					// Clean up resources on error
+					size = 0;
+					delete[] map;
 					throw;
 				}
 			};
@@ -113,33 +140,12 @@ namespace hypermap {
 			return *this;
 		};
 		
-		
-		// Constants for quadratic probing
-		inline static const uint8_t c1 = 1;
-		inline static const uint8_t c2 = 3;
-		
 		variant<Derived_T...>& operator[](const string& key) {
-			// Calculate initial hash
-			uint32_t hash = hyperhash::hash(key);
-			// Trimm down hash to index
-			uint32_t idx = hash & (size-1);
-			// Set attempt to 0
-			uint32_t att = 0;
-
-			// Initial slot
-			HyperSlot<variant<Derived_T...>>* slot = &map[idx];
-
-			// Probe until slot is uninitialized || the correct slot
-			while (slot->key!=key && slot->key!="") {
-				// Quadratic probing function
-				idx = (hash + c1 * att + c2 * (att * att)) % size;
-				// Update slot
-				slot = &map[idx];
-				// Increment attempt (if every field was probed -> nullptr)
-				if (++att>size) throw runtime_error("HyperMap exhausted; No free slot found!");
-			}
+			HyperSlot<variant<Derived_T...>>* slot = probe(key, size, map);
+			if (!slot) throw runtime_error("HyperMap exhausted; No free slot found!");
 
 			if (slot->key=="") {
+				// Update slot values (assignment operator must deallocate old resources if type is correctly implemented)
 				slot->key = key;
 				slot->val = variant<Derived_T...>();
 			}
@@ -148,51 +154,17 @@ namespace hypermap {
 		};
 
 		Base_T* get(const string& key) {
-			// Calculate initial hash
-			uint32_t hash = hyperhash::hash(key);
-			// Trimm down hash to index
-			uint32_t idx = hash & (size-1);
-			// Set attempt to 0
-			uint32_t att = 0;
-
-			// Initial slot
-			HyperSlot<variant<Derived_T...>>* slot = &map[idx];
-
-			// Probe until slot is uninitialized || the correct slot
-			while (slot->key!=key && slot->key!="") {
-				// Quadratic probing function
-				idx = (hash + c1 * att + c2 * (att * att)) % size;
-				// Update slot
-				slot = &map[idx];
-				// Increment attempt (if every field was probed -> nullptr)
-				if (++att>size) return nullptr;
-			}
+			HyperSlot<variant<Derived_T...>>* slot = probe(key, size, map);
+			if (!slot) return nullptr;
 			// Return BasePointer or nullptr if slot is empty
 			return slot->key=="" ? nullptr : visit(BaseVisitor<Base_T>{}, slot->val);
 		};
 
 		Base_T* set(const string& key, const variant<Derived_T...>& val) {
-			// Calculate initial hash
-			uint32_t hash = hyperhash::hash(key);
-			// Trimm down hash to index
-			uint32_t idx = hash & (size-1);
-			// Set attempt to 0
-			uint32_t att = 0;
+			HyperSlot<variant<Derived_T...>>* slot = probe(key, size, map);
+			if (!slot) throw runtime_error("HyperMap exhausted; No free slot found!");
 
-			// Initial slot
-			HyperSlot<variant<Derived_T...>>* slot = &map[idx];
-
-			// Probe until slot is uninitialized || the correct slot
-			while (slot->key!="" && slot->key!=key) {
-				// Quadratic probing function
-				idx = (hash + c1 * att + c2 * (att * att)) % size;
-				// Update slot
-				slot = &map[idx];
-				// Increment attempt (if every field was probed -> nullptr)
-				if (++att>size) throw runtime_error("HyperMap exhausted; No free slot found!");
-			}
-
-			// Update slot values
+			// Update slot values (assignment operator must deallocate old resources if type is correctly implemented)
 			slot->key = key;
 			slot->val = val;
 
@@ -200,6 +172,21 @@ namespace hypermap {
 		};
 
 		void del(const string& key) {
+			HyperSlot<variant<Derived_T...>>* slot = probe(key, size, map);
+			if (!slot) return;
+
+			// Update slot values (assignment operator must deallocate old resources if type is correctly implemented)
+			slot->key = "";
+			slot->val = HyperSlot<variant<Derived_T...>>();
+		};
+		
+	private:
+		// Constants for quadratic probing
+		inline static const uint8_t c1 = 1;
+		inline static const uint8_t c2 = 3;
+
+		// Function for probing / finding the requested key
+		inline static HyperSlot<variant<Derived_T...>>* probe(const string& key, const uint16_t& size, const HyperSlot<variant<Derived_T...>>* map) {
 			// Calculate initial hash
 			uint32_t hash = hyperhash::hash(key);
 			// Trimm down hash to index
@@ -216,14 +203,11 @@ namespace hypermap {
 				idx = (hash + c1 * att + c2 * (att * att)) % size;
 				// Update slot
 				slot = &map[idx];
-				// Increment attempt (if every field was probed -> nullptr)
-				if (++att>size) return;
+				// Increment attempt (if every field was probed -> return)
+				if (++att>size) return nullptr;
 			}
-
-			slot->key = "";
-			slot->val = HyperSlot<variant<Derived_T...>>();
 		};
-	private:
+		
 		uint16_t size;
 		HyperSlot<variant<Derived_T...>>* map;
 	};
