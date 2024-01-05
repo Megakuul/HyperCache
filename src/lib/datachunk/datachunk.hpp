324f6e9e193f7432a1757c70ae6f0a8b7187238a
Megakuul <megakuul@gmx.ch>
2024-01-05 19:14:41 +0100

    Implemented ProtoChunk datatype

--- src/lib/datachunk/datachunk.hpp
+++ src/lib/datachunk/datachunk.hpp
@@ -21,93 +21,249 @@
 #define DATA_CHUNK_H
 
 #include <cstdint>
+#include <limits>
+#include <cstring>
 #include <memory>
+#include <stdexcept>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-class ProtoChunk;
-class CountChunk;
-class GroupChunk;
+namespace datachunk {
 
-class DataChunk {
-public:
-	virtual int8_t get_type() { return -1; };
-	
-  virtual vector<uint8_t>* get_proto() { return nullptr; };
-  virtual vector<uint8_t>* set_proto(vector<uint8_t>* new_bytes) { return nullptr; };
-  
-  virtual uint64_t* get_count() { return nullptr; };
-  virtual uint64_t* set_count(uint64_t* new_count) { return nullptr; };
-	virtual uint64_t* inc_count(int64_t* inc_count) { return nullptr; };
-
-  virtual unordered_set<string>* get_group() { return nullptr; };
-	virtual unordered_set<string>* set_group(unordered_set<string>* new_group) { return nullptr; };
-  virtual unordered_set<string>* push_group(string* key) { return nullptr; };
-	virtual unordered_set<string>* del_group(string* key) { return nullptr; };
-	
-private:
-	unordered_set<weak_ptr<GroupChunk>> group_assignments; 
-};
-
-class ProtoChunk : public DataChunk {
-public:
-	int8_t get_type() override { return 0; };
-	
-  vector<uint8_t>* get_proto() override {
-		return &bytes;
-  };
-  vector<uint8_t>* set_proto(vector<uint8_t>* new_bytes) override {
-		bytes = *new_bytes;
-		return &bytes;
-  };
-private:
-  vector<uint8_t> bytes;
-};
-  
-class CountChunk : public DataChunk {
-public:
-	int8_t get_type() override { return 1; };
-	
-  uint64_t* get_count() override {
-		return &count;
-  };
-  uint64_t* set_count(uint64_t* new_count) override {
-		count = *new_count;
-		return &count;
-  };
-	uint64_t* inc_count(int64_t* inc_count) override {
-		count += *inc_count;
-		return &count;
+	/**
+	 * DataType of a DataChunk
+	 */
+	enum DataType {
+		NONE = -1,
+		PROTO = 0,
+		COUNT = 1,
+		GROUP = 2
 	};
-private:
-  uint64_t count = 0;
-};
-
-class GroupChunk : public DataChunk {
-public:
-	int8_t get_type() override { return 2; };
 	
-  unordered_set<string>* get_group() override {
-		return &group;
-  };
-  unordered_set<string>* set_group(unordered_set<string>* new_group) override {
-		group = *new_group;
-		return &group;
-  };
-	unordered_set<string>* push_group(string* key) override {
-		group.insert(*key);
-		return &group;
+	class ProtoChunk;
+	class CountChunk;
+	class GroupChunk;
+
+	/**
+	 * DataChunk is a BaseClass for the different datatypes in HyperCache.
+	 *
+	 * The BaseClass implements virtual functions for all datatypes.
+	 * To access a datatype, usually a BaseClass pointer is returned from HyperMap,
+	 * this pointer can be used to directly access the datatype.
+	 * If the datatype you want to access is not implemented, a runtime_error is thrown,
+	 * this exception can directly be returned to the client
+	 * (as it is the clients fault if the wrong datatype is accessed).
+	 */
+	class DataChunk {
+	public:
+		/**
+		 * Get DataType of the implemented type
+		 */
+		virtual DataType get_type() noexcept { return NONE; };
+
+		/**
+		 * Get ProtoChunk data (for informations check ProtoChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not ProtoChunk
+		 */
+		virtual pair<const uint8_t*, const uint8_t> get_proto() {
+			throw runtime_error("DataChunk is not of type PROTO");
+		};
+		/**
+		 * Set ProtoChunk data (for informations check ProtoChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not ProtoChunk
+		 */
+		virtual pair<const uint8_t*, const uint8_t> set_proto(uint8_t* new_bytes, uint8_t size) {
+			throw runtime_error("DataChunk is not of type PROTO");
+		};
+
+		/**
+		 * Get CountChunk data (for informations check CountChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not CountChunk
+		 */
+		virtual uint64_t get_count() {
+			throw runtime_error("DataChunk is not of type COUNT");
+		};
+		/**
+		 * Set CountChunk data (for informations check CountChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not CountChunk
+		 */
+		virtual uint64_t set_count(uint64_t& new_count) {
+			throw runtime_error("DataChunk is not of type COUNT");
+		};
+		/**
+		 * Increment CountChunk data (for informations check CountChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not CountChunk
+		 */
+		virtual uint64_t inc_count(int64_t& inc_count) {
+			throw runtime_error("DataChunk is not of type COUNT");
+		};
+
+		/**
+		 * Get GroupChunk data (for informations check GroupChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not GroupChunk
+		 */
+		virtual unordered_set<string> get_group() {
+			throw runtime_error("DataChunk is not of type GROUP");
+		};
+		/**
+		 * Set GroupChunk data (for informations check GroupChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not GroupChunk
+		 */
+		virtual unordered_set<string> set_group(unordered_set<string>& new_group) {
+			throw runtime_error("DataChunk is not of type GROUP");
+		};
+		/**
+		 * Push to GroupChunk data (for informations check GroupChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not GroupChunk
+		 */
+		virtual unordered_set<string> push_group(string& key) {
+			throw runtime_error("DataChunk is not of type GROUP");
+		};
+		/**
+		 * Delete from GroupChunk data (for informations check GroupChunk)
+		 *
+		 * Throws a runtime_error if the derived type is not GroupChunk
+		 */
+		virtual unordered_set<string> del_group(string& key) {
+			throw runtime_error("DataChunk is not of type GROUP");
+		};
+	
+	private:
+		// unordered_set<weak_ptr<GroupChunk>> group_assignments;
 	};
-	unordered_set<string>* del_group(string* key) override {
-	  group.erase(*key);
-		return &group;
+
+	/**
+	 * ProtoChunk the default Datatype for HyperCache.
+	 *
+	 * The ProtoChunk is designed to contain raw bytes, usually this is a datastructure
+	 * that has been serialized with Protobuffers on the client side.
+	 *
+	 * For fast read/writes of simple ProtoChunk values, this type implements two ways to store data.
+	 *
+	 * If the data fits into the quick_bytes slot (usually 255Bytes) it is directly inserted there.
+	 * The quick_bytes slot is fixed allocated in the class metadata,
+	 * this allows reading/writing with 0 runtime heap operations (making it very fast).
+	 *
+	 * If the data does not fit, it is automatically allocated in a regular vector object. 
+	 */
+	class ProtoChunk : public DataChunk {
+	public:
+		DataType get_type() noexcept override { return PROTO; };
+	
+		pair<const uint8_t*, const uint8_t> get_proto() override {
+			// If quick_mode is enabled, return quick bytes
+			if (quick_mode) return make_pair(quick_bytes, quick_size);
+			// If not, return the bytes from vector
+			else return make_pair(bulk_bytes.data(), bulk_bytes.size());
+		};
+		
+		pair<const uint8_t*, const uint8_t> set_proto(uint8_t* new_bytes, uint8_t size) override {
+			if (size < quick_cap) {
+				// Proto fits into the quick_bytes
+				
+				if (!quick_mode) {
+					// If bulk was enabled before, deallocate bulk_bytes
+					bulk_bytes = vector<uint8_t>();
+					quick_mode = true;
+				}
+				// Update size
+				quick_size = size;
+				// Raw copy to the quick_bytes 
+				memcpy(quick_bytes, new_bytes, quick_size);
+				// Return byte ptr + size
+				return make_pair(quick_bytes, quick_size);
+			} else {
+				// Proto does not fit into quick_bytes
+				
+				quick_mode = false;
+				// Assign bytes to vector (basically reinitializes vector)
+				bulk_bytes.assign(new_bytes, new_bytes + size);
+				// Return byte ptr + size
+				return make_pair(bulk_bytes.data(), bulk_bytes.size());
+			}
+		};
+	private:
+		// Capacity of quick field using uint8_t (255) bytes, with a 16K map, runtime overhead is at around 5MB
+		// This seems much, but is a fine tradeoff regarding that most proto requests will fit into this.
+		static constexpr uint_fast8_t quick_cap = numeric_limits<uint8_t>::max();
+		// State of quick_mode
+		bool quick_mode = true;
+		// Size of the quick field
+		uint8_t quick_size = 0;
+		// quick_bytes array
+		uint8_t quick_bytes[quick_cap];
+		// bulk_bytes array
+		vector<uint8_t> bulk_bytes;
 	};
-private:
-  unordered_set<string> group;
+
+	/**
+	 * CountChunk is a additional Datatype for HyperCache.
+	 *
+	 * This datatype is a simple counter that can be incremented / decremented.
+	 *
+	 * If the counter overflows (unsigned 64bit), expected modulo semantics come into force.
+	 * (e.g. 2^64+1 = 1 || 0-1 = 2^64-1)
+	 */
+	class CountChunk : public DataChunk {
+	public:
+		DataType get_type() noexcept override { return COUNT; };
+	
+		uint64_t get_count() override {
+			return count;
+		};
+		uint64_t set_count(uint64_t& new_count) override {
+			count = new_count;
+			return count;
+		};
+		uint64_t inc_count(int64_t& inc_count) override {
+			count += inc_count;
+			return count;
+		};
+	private:
+		uint64_t count = 0;
+	};
+
+	/**
+	 * GroupChunk is a additional Datatype for HyperCache.
+	 *
+	 * This datatype is a group of datachunks.
+	 */
+	class GroupChunk : public DataChunk {
+	public:
+		DataType get_type() noexcept override { return GROUP; };
+	
+		unordered_set<string> get_group() override {
+			return group;
+		};
+		unordered_set<string> set_group(unordered_set<string>& new_group) override {
+			group = new_group;
+			return group;
+		};
+		unordered_set<string> push_group(string& key) override {
+			group.insert(key);
+			return group;
+		};
+		unordered_set<string> del_group(string& key) override {
+			group.erase(key);
+			return group;
+		};
+	private:
+		unordered_set<string> group;
+	};
+
 };
 
 #endif
